cdf90a72eb5f955053387c22e7e9124cd9e30cfc
LegitWIZRD <101444032+LegitWIZRD@users.noreply.github.com>
2023-03-27 10:36:16 -0500

    Added a Tickets database and populated the data to the home page for logged-in users.

--- TicketManager3/APP/models.py
+++ TicketManager3/APP/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    entry_1 = models.CharField(max_length=50)
+    entry_2 = models.CharField(max_length=20)
+    entry_3 = models.CharField(max_length=40)
+    entry_4 = models.CharField(max_length=10)
+    entry_5 = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.created_at} {self.entry_1} {self.entry_2} {self.entry_3} {self.entry_4} {self.entry_5}"
--- TicketManager3/APP/views.py
+++ TicketManager3/APP/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout, login
 from django.contrib import messages
+from .models import Ticket
 # Create your views here.
 
 
 def home(request):
+    tickets = Ticket.objects.all()
     if request.method == 'POST':
         username = request.POST["usr"]
         password = request.POST["pswd"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "You have been successfully logged in!")
             return redirect('home')
         else:
             messages.success(request, "The user name or password provided is incorrect!")
             return redirect('home')
     return render(request, 'home.html', {
-        'home': home
+        'home': home,
+        'tickets': tickets
     })
 
 
