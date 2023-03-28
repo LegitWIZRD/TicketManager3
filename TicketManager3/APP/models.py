e27d019c841676c6d787b4355f8275cbb2acf99c
LegitWIZRD <101444032+LegitWIZRD@users.noreply.github.com>
2023-03-27 19:24:43 -0500

    Updated the database field names to be more like the real-world fields that will be used. Updated the html files to include the new database fields names, and also adjusted the field length values. Email subject columns were added to the home page and ticket page for easy copy/pasting.

--- TicketManager3/APP/models.py
+++ TicketManager3/APP/models.py
@@ -5,11 +5,13 @@ from django.db import models
 
 class Ticket(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    entry_1 = models.CharField(max_length=50)
-    entry_2 = models.CharField(max_length=20)
-    entry_3 = models.CharField(max_length=40)
-    entry_4 = models.CharField(max_length=10)
-    entry_5 = models.CharField(max_length=30)
+    ticket_number = models.CharField(max_length=50)
+    contact_name = models.CharField(max_length=20)
+    contact_email = models.EmailField(max_length=40)
+    description = models.CharField(max_length=50)
+    notes = models.CharField(max_length=1000)
 
     def __str__(self):
-        return f"{self.created_at} {self.entry_1} {self.entry_2} {self.entry_3} {self.entry_4} {self.entry_5}"
+        return f"{self.created_at} {self.ticket_number} {self.contact_name} {self.contact_email} {self.description} {self.notes}"
+
+
